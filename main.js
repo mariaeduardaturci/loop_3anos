0694fe4c31507b6027e1cddea7f10727c032c21f
MARIA TURCI <107404545+mariaeduardaturci@users.noreply.github.com>
2024-04-16 11:36:30 -0300

    loop2

--- main.js
+++ main.js
@@ -5,14 +5,14 @@ function verificarTime() {
 
   while (continuarPerguntando) {
     
-    var respostaTime = prompt("Qual o melhor time?");
+    var respostaTime = prompt("insira a senha de 8 digitos");
 
     
-    if (respostaTime.toLowerCase() === "athlético pr") {
-      alert("Isso mesmo! O Athlético PR é o melhor time!");
+    if (respostaTime.toLowerCase() === "19278406") {
+      alert("senha correta");
       continuarPerguntando = false; // Sai do loop após a resposta correta
     } else {
-      alert("Não é isso! Tente novamente.");
+      alert("senha incorreta, tente novamente");
     }
   }
 }
